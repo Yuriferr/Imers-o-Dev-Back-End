81fe2a1f46af00733727c2555c9260759ea135c2
Yuriferr <yurifernandespreto@gmail.com>
2024-12-04 11:23:27 -0300

    projeto completo

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+app.use(express.static("uploads"));
+routes(app);
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Hello World!");
-});
+
